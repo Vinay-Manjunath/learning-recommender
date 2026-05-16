3e74593cc9f82bb6e0315be67e1a93ae2de2c8fc
Vinay Manjunath <146633466+Vinay-Manjunath@users.noreply.github.com>
2026-05-16 19:32:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     try {
 
       const response = await axios.post(
-        "http://localhost:8000/recommend",
+        "http://localhost:30880/recommend",
         {
           query,
           difficulty,
